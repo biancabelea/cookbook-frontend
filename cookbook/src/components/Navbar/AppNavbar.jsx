fe734b3f710ad5f1eb8774f84a5773ba074c4eb8
Bianca Belea <bianca.belea@student.upt.ro>
2022-01-22 15:08:42 +0200

    refactor: change language in menu

--- cookbook/src/components/Navbar/AppNavbar.jsx
+++ cookbook/src/components/Navbar/AppNavbar.jsx
@@ -5,12 +5,13 @@ function AppNavbar () {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Home</Navbar.Brand>
+                <Navbar.Brand href="#home">Acasă</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="addRecipePage">Add your recipe</Nav.Link>
-                        <Nav.Link href="#login">Login</Nav.Link>
+                        <Nav.Link href="#addRecipePage">Adaugă o rețetă</Nav.Link>
+                        <Nav.Link href="#seeRecipes">Vezi rețetele</Nav.Link>
+                        <Nav.Link href="#login">Autentificare</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
